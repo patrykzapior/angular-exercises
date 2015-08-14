d20e78ef651a2e5c3c4dd7a789d122417a3a8da3
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-14 09:17:25 +0200

    Bind Posts

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -3,9 +3,13 @@
     'use strict';
     function BlogPostCtrl()
     {
+        var ctrl = this;
+        ctrl.posts = [
+            { id: 1, author: 'Jack', title: 'Diving Deep with Dependency Injection' },
+            { id: 2, author: 'Jill', title: 'Practical End-to-End Testing with Protractor'}
+        ]
 
     }
-
     var module = angular.module("exerciseApp", []);
     module.controller('BlogPostCtrl', [BlogPostCtrl]);
 })();
