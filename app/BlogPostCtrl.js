8cb606ae20a33ad8ea57befb9a6fa53157052b38
zapiorpatryk <zapior.patryk@gmail.com>
2015-08-14 09:40:55 +0200

    Bind Posts From DAO

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -3,7 +3,10 @@
     'use strict';
     function BlogPostCtrl(PostDAO)
     {
-
+        var ctrl = this;
+        PostDAO.query().then(function(data){
+            ctrl.posts = data;
+        })
     }
 
     var module = angular.module('exerciseApp');
